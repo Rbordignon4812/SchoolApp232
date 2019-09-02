0857207e16ff8e66b5b3ae21a38f2f3e09103312
Roger Bordignon <twario4812@gmail.com>
2019-09-02 12:23:39 -0400

    Roger Bordignon 2306445

---
+++ app/src/main/java/com/example/schoolapp2_3/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.schoolapp2_3;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, schoolInfo.class));
+            }
+        });
+    }
+}
